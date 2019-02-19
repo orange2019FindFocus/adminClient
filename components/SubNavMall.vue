0f2e9087123d03cb644e39518426985c9cbe701a
lucong <332553882@qq.com>
2019-02-19 20:50:43 +0800

    ad

--- components/SubNavMall.vue
+++ components/SubNavMall.vue
@@ -7,6 +7,8 @@
       <v-btn flat to="/mall/goods">商品列表</v-btn>
       <v-btn flat to="/mall/category">分类管理</v-btn>
       <v-btn flat to="/mall/payment">账单记录</v-btn>
+      <v-btn flat to="/mall/orderItems">评价记录</v-btn>
+      <v-btn flat to="/mall/orderRates">售后申请</v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </template>
