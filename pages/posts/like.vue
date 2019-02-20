1d34f31a6a4632c4f52067bc21f226458bf3a979
DESKTOP-P635RSB\Think <zhangyunfeng0101@gmail.com>
2019-02-20 17:36:15 +0800

    feat: 完善用户点赞列表

--- pages/posts/like.vue
+++ pages/posts/like.vue
@@ -14,18 +14,19 @@
             @keypress="searchList"
           ></v-text-field>-->
         </v-card-title>
-        <v-data-table :headers="table.headers" :items="listDatas" class="elevation-1" hide-actions>
+        <v-data-table :headers="table.headers" :items="likesList" class="elevation-1" hide-actions>
           <template slot="items" slot-scope="props">
             <td>{{ props.item.id }}</td>
-            <td>{{ props.item.post.title }}</td>
-            <td>{{ props.item.user_info.mobile }}</td>
+            <td>{{ props.item.post ? props.item.post.title : '【未知文章】' }}</td>
             <td>{{ props.item.user_info.nickname }}</td>
+            <td>{{ props.item.user_info.mobile }}</td>
+            <td>{{ props.item.ip }}</td>
             <td>{{ dateFormat(props.item.create_time) }}</td>
           </template>
         </v-data-table>
 
         <div class="pt-2 pb-2">
-          <v-pagination v-model="page" :length="listPageLength" @input="pageChange"></v-pagination>
+          <v-pagination v-model="table.page" :length="listPageLength" @input="pageChange"></v-pagination>
         </div>
       </v-card>
     </v-flex>
@@ -51,17 +52,18 @@ export default {
       table: {
         headers: [
           { text: "ID", sortable: false, value: "id" },
-          { text: "标题", value: false, sortable: false },
-          { text: "用户", value: false, sortable: false },
-          { text: "", value: false, sortable: false },
-          { text: "时间", value: false, sortable: false }
+          { text: "文章标题", value: false, sortable: false },
+          { text: "用户昵称", value: false, sortable: false },
+          { text: "用户手机号", value: false, sortable: false},
+          { text: "IP地址", value: false, sortable: false },
+          { text: "点赞时间", value: false, sortable: false }
         ]
       },
       page: parseInt(this.$route.query.page) || 1
     };
   },
   computed: {
-    listDatas() {
+    likesList() {
       return this.$store.state.postsLike.list;
     },
     listCount() {
